fdd692936502c237d4e8eb190717d433f3a68997
Manuel <mangonzalez12@gmail.com>
2020-02-16 15:53:24 +0100

    commit

--- Linear Regression practice.R
+++ Linear Regression practice.R
@@ -16,3 +16,39 @@ fit1<-lm(Boston$medv~Boston$lstat, Boston)
 fit1
 summary(fit1)
 attach(Boston)
+fit1$coefficients
+fit1$residuals
+
+hist(fit1$residuals)
+fit1$effects
+fit1$rank
+fit1$fitted.values
+fit1$assign
+fit1$qr
+fit1$df.residual
+
+
+####Prediction, remember that prediction treats the function as a black box
+
+predict(fit1, data.frame(lstat=c(5,10,15)), interval="confidence")
+predict(fit1, data.frame(lstat=c(5,10,15)), interval = "prediction")
+
+####Plots
+
+plot(lstat~medv)
+abline(fit1)       
+
+abline(fit1, lwd=3)
+abline(fit1, lwd=3, col="red")
+plot(lstat, medv, col="red")
+plot(lstat, medv, pch=20)
+plot(lstat, medv, pch="+")
+plot(1:20, 1:20, pch=1:20)
+
+######Make various graphs appear in the same plot screen
+par(mfrow=c(2,2))
+plot(fit1)
+
+plot(predict(fit1, residuals(fit1)
+plot(predict(fit1), rstudent(fit1))
+plot(hatvalues(fit1))
