06c6779ea0edc3ce387d80e925e44c83b47d9914
Manuel <mangonzalez12@gmail.com>
2020-04-23 23:49:50 +0200

    puta

--- Linear Regression practice.R
+++ Linear Regression practice.R
@@ -12,10 +12,28 @@ hist(Boston$chas)
 hist(Boston$nox)
 hist(Boston$rm)
 
+extract.data<-function(chas, indus){
+  new.data<-cbind(chas, indus)
+  return(new.data)
+}
+x<-extract.data(chas, indus)
+x
+head(x)
+
+Boston
+
+
+
 fit1<-lm(Boston$medv~Boston$lstat, Boston)
 fit1
 summary(fit1)
 plot(Boston$medv~Boston$lstat)
+
+abline(fit1, lwd=3)
+
+fit2<-lm(Boston$medv~Boston$lstat, Boston$chas, Boston)
+
+
 
 cor(Boston$medv,Boston$lstat)
 cor.test(Boston$medv,Boston$lstat)
@@ -67,3 +85,42 @@ describe(Boston)
 plot(Boston$crim)
 install.packages("psych")
 describe(Boston)
+
+
+
+#####Practice IF, loop, while loop
+
+data<-cbind(x,y)
+x
+y<-c(4,5,6,7,8)
+data<-cbind(x,y)
+data
+data[[1]]
+data[1,]
+
+data[,1]
+class(data)
+as.data.frame(data)
+subset.data<-if(as.data.frame(data)[,1]>2){
+  print(x)
+}
+is.na(x)
+bad<-is.na(x)
+x[!bad]
+
+for (i in data[,1]) {
+  i=i+2
+  print(i)
+}
+
+number<-0
+while(data[,1])>2{
+  print(number)
+}
+
+for (a in data[,1]) {
+  a<-a*a
+  print(a)
+}
+
+
